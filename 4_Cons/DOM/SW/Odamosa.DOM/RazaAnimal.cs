803f62d5958513d0d45a65cc219f0da9223931a1
SrMiyagi <nicolas.marin.c@hotmail.com>
2013-09-08 18:18:11 -0300

    Conflicto

--- 4_Cons/DOM/SW/Odamosa.DOM/RazaAnimal.cs
+++ 4_Cons/DOM/SW/Odamosa.DOM/RazaAnimal.cs
@@ -9,7 +9,7 @@ namespace Odamosa.DOM
     {
         #region
 
-        public string nombreRaza { get; set; }
+        public string nombreRaza1 { get; set; }
         public int codEspecie { get; set; }
         public String nombreEspecie { get; set; }
         //Nicolas
